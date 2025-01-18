71fe82f9e2f6c5c46eca8d02608dc97d3f621e68
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-18 16:17:07 +1100

    Updated IPs + added speedtest

--- real-debrid-network-test.sh
+++ real-debrid-network-test.sh
@@ -1,64 +1,31 @@
 #!/bin/bash
 
-# set -x
-
 # Color variables
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Start of the script
-echo -e "${YELLOW}Starting REAL-DEBRID network tests...${NC}"
-echo "https://github.com/debridmediamanager/real-debrid-network-test"
-
 ips=(
-  "20.download.real-debrid.com"
-  "21.download.real-debrid.com"
-  "22.download.real-debrid.com"
-  "23.download.real-debrid.com"
-  "30.download.real-debrid.com"
-  "31.download.real-debrid.com"
-  "32.download.real-debrid.com"
-  "34.download.real-debrid.com"
-  "40.download.real-debrid.com"
-  "41.download.real-debrid.com"
-  "42.download.real-debrid.com"
-  "43.download.real-debrid.com"
-  "44.download.real-debrid.com"
-  "45.download.real-debrid.com"
-  "50.download.real-debrid.com"
-  "51.download.real-debrid.com"
-  "52.download.real-debrid.com"
-  "53.download.real-debrid.com"
-  "54.download.real-debrid.com"
-  "55.download.real-debrid.com"
-  "56.download.real-debrid.com"
-  "57.download.real-debrid.com"
-  "58.download.real-debrid.com"
-  "59.download.real-debrid.com"
-  "60.download.real-debrid.com"
-  "61.download.real-debrid.com"
-  "62.download.real-debrid.com"
-  "63.download.real-debrid.com"
-  "64.download.real-debrid.com"
-  "65.download.real-debrid.com"
-  "66.download.real-debrid.com"
-  "67.download.real-debrid.com"
-  "68.download.real-debrid.com"
-  "69.download.real-debrid.com"
-  "hkg1.download.real-debrid.com"
-  "lax1.download.real-debrid.com"
-  "lon1.download.real-debrid.com"
-  "mum1.download.real-debrid.com"
-  "rbx.download.real-debrid.com"
-  "sgp1.download.real-debrid.com"
-  "tlv1.download.real-debrid.com"
-  "tyo1.download.real-debrid.com"
+"45-6.download.real-debrid.com"
+"45-4.download.real-debrid.com"
+"rbx-6.download.real-debrid.com"
+"rbx-4.download.real-debrid.com"
+"syd1-6.download.real-debrid.com"
+"syd1-4.download.real-debrid.com"
+"hkg1-6.download.real-debrid.com"
+"hkg1-4.download.real-debrid.com"
+"sgp1-6.download.real-debrid.com"
+"sgp1-4.download.real-debrid.com"
+"sgpo1-6.download.real-debrid.com"
+"sgpo1-4.download.real-debrid.com"
+"mum1-6.download.real-debrid.com"
+"mum1-4.download.real-debrid.com"
+"jkt1-6.download.real-debrid.com"
+"jkt1-4.download.real-debrid.com"
 )
 
 tempfile=$(mktemp)
-echo -e "${GREEN}Temporary file created at $tempfile${NC}"
 
 traceroute_and_parse() {
   local ip=$1
@@ -71,47 +38,34 @@ traceroute_and_parse() {
   if [ $? -eq 0 ]; then
     hops=$(echo "$output" | wc -l | tr -d ' ')
     latency=$(echo "$output" | tail -1 | awk '{print $3}' | tr -d 'ms')
-
-    echo "$ip $hops $latency" >> "$tempfile"
+    speed=$(curl -s -m 10 -o /dev/null -w "%{size_download}" "$ip/speedtest/testDefault.rar" | awk '{bytes=$1; bits=bytes*8; megabits=bits/1000000/10; print megabits}')
+    echo -e "Host: ${GREEN}$ip${NC}, Hops: ${GREEN}$hops${NC}, Latency: ${GREEN}${latency}ms${NC}, Speed: ${GREEN}${speed}Mbps${NC}"
+    echo "$ip $hops $latency $speed" >> "$tempfile"
   else
     echo "Error performing traceroute for $ip" >&2
   fi
 }
 
-batch_traceroute() {
-  local batch_size=10
-  local count=0
-
+traceroute_speed() {
   for ip in "${ips[@]}"; do
-    traceroute_and_parse "$ip" &
-    ((count++))
-
-    if (( count % batch_size == 0 )); then
-      wait
-      count=0
-    fi
+    traceroute_and_parse "$ip" 
   done
-  echo "Waiting for the final batch to complete..."
-  wait
 }
 
-batch_traceroute
+
+echo -e "${YELLOW}Starting tests...${NC}"
+traceroute_speed
 
 echo -e "${YELLOW}Sorting results...${NC}"
 sort -n -k 2,2 -k 3,3 "$tempfile" > "${tempfile}_sorted"
-
-echo -e "${YELLOW}Calculating latency threshold...${NC}"
 lowest_latency=$(awk 'NR==1{print $3}' "${tempfile}_sorted")
 latency_threshold=$(bc <<< "$lowest_latency + ($lowest_latency / 4)")
-
-echo -e "${YELLOW}Displaying hosts within latency threshold:${NC}"
 while IFS= read -r line; do
-  read -r ip hops latency <<< "$line"
+  read -r ip hops latency speed <<< "$line"
   if (( $(bc <<< "$latency <= $latency_threshold") )); then
-    echo -e "Host: ${GREEN}$ip${NC}, Hops: ${GREEN}$hops${NC}, Latency: ${GREEN}${latency}ms${NC}"
+    echo -e "Host: ${GREEN}$ip${NC}, Hops: ${GREEN}$hops${NC}, Latency: ${GREEN}${latency}ms${NC}, Speed: ${GREEN}${speed}Mbps${NC}"
   fi
-done < "${tempfile}_sorted"
+  done < "${tempfile}_sorted"
 
-echo -e "${YELLOW}Cleaning up temporary files...${NC}"
 rm "$tempfile" "${tempfile}_sorted"
-echo -e "${GREEN}Temporary files removed. Network test operation complete.${NC}"
+
